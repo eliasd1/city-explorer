a6623674e96cf2824f83add616fe583317436177
eliasd1 <elidadoush23@gmail.com>
2021-02-22 14:17:28 +0200

    refactored location path

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 'use strict'
 
-let express = require('express')
+let express = require('express');
 let cors = require("cors");
-
+let superagent = require("superagent");
 let app = express();
 require("dotenv").config()
 
@@ -16,59 +16,75 @@ app.get("/weather", handleWeather);
 
 app.get("*", handle404);
 
-app.listen(PORT, ()=>{
+app.listen(PORT, () => {
     console.log("Listening on port " + PORT);
 })
 
 
-function CityLocation(searchQuery, displayName, lat, lon){
+function CityLocation(searchQuery, displayName, lat, lon) {
     this.search_query = searchQuery;
     this.formatted_query = displayName;
     this.latitude = lat;
     this.longitude = lon;
 }
-function Weather(forecast, time){
+function Weather(forecast, time) {
     this.forecast = forecast;
     this.time = time;
 }
-function handleLocation(req, res){
-    try{
+function handleLocation(req, res) {
+    try {
         let searchQuery = req.query.city;
-        let locationObject = getLocationData(searchQuery);
-        res.status(200).send(locationObject);
-    }catch(error){
+        getLocationData(searchQuery).then(data => {
+            res.status(200).send(data);
+        });
+    } catch (error) {
         res.status(500).send(errorHandler())
     }
 }
 
-function getLocationData(searchQuery){
-    let locationData = require("./data/location.json");
-    let latitude = locationData[0].lat;
-    let longitude = locationData[0].lon;
-    let displayName = locationData[0].display_name;
-    let responseObject = new CityLocation(searchQuery, displayName, latitude, longitude)
-    return responseObject;
+function getLocationData(searchQuery) {
+    // let locationData = require("./data/location.json");
+    let url = "https://eu1.locationiq.com/v1/search.php"
+    const query = {
+        key: process.env.GEO_CODE_KEY,
+        q: searchQuery,
+        limit:1,
+        format: "json"
+    }
+    return superagent.get(url).query(query).then(data => {
+        console.log(data.body);
+        try {
+            let latitude = data.body[0].lat;
+            let longitude = data.body[0].lon;
+            let displayName = data.body[0].display_name;
+            let responseObject = new CityLocation(searchQuery, displayName, latitude, longitude)
+            return responseObject;
+        } catch{
+            console.log("Somethhing is wrong")
+        }
+        
+    }).catch(error => console.log(error))
 }
 
-function handleWeather(req, res){
-    try{
+function handleWeather(req, res) {
+    try {
         res.status(200).send(getWeatherData())
-    } catch{
+    } catch {
         res.status(500).send(errorHandler())
     }
 }
 
-function getWeatherData(){
+function getWeatherData() {
     let weatherData = require("./data/weather.json").data;
     return weatherData.map(day => new Weather(day.weather.description, new Date(day.datetime).toDateString()));
 }
 
-function errorHandler(){
-    return{
-        status:500,
+function errorHandler() {
+    return {
+        status: 500,
         textResponse: "Sorry, something went wrong"
     }
 }
-function handle404(){
+function handle404() {
     res.status(404).send("Path noth found");
 }
