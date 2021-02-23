dc6e1a075ab1ee816c40862bb7f8d1e58c875d8c
eliasd1 <elidadoush23@gmail.com>
2021-02-23 16:47:26 +0200

    change client variable

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ let pg = require("pg");
 require("dotenv").config()
 app.use(cors());
 const PORT = process.env.PORT
-let client = new pg.Client(process.env.DATABASE_URL);
+const client = new pg.Client({ connectionString: process.env.DATABASE_URL,   ssl: { rejectUnauthorized: false } });
 
 
 
